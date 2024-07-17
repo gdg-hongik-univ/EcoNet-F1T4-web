c3dac6c2ff8131f015bcb5f6db37ad68bb7eac30
05ovo2e <jiwonpark2229@gmail.com>
2024-07-17 14:31:01 +0900

    0717 SettingPage

---
+++ 05ovo2e/src/Main.js
@@ -0,0 +1,12 @@
+import Mainpage from "./pages/Mainpage.js";
+import SettingPage from "./pages/SettingPage";
+
+function Main() {
+  return (
+    <div>
+      <SettingPage></SettingPage>
+    </div>
+  );
+}
+
+export default Main;
---
+++ 05ovo2e/src/components/Logo.js
@@ -0,0 +1,5 @@
+import "../styles/logo.css";
+function Logo() {
+  return <div className="logo-style">로고 자리임</div>;
+}
+export default Logo;
