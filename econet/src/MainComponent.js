5c25ba7fe76382b0388ea827ca5a48e61b79b9d4
minseo6753 <109271028+minseo6753@users.noreply.github.com>
2024-07-31 14:59:13 +0900

    Update MainComponent.js (Mainpage to MainPage)

--- econet/src/MainComponent.js
+++ econet/src/MainComponent.js
@@ -4,7 +4,7 @@ import { ThemeProvider } from "@mui/material";
 
 // pages
 
-import Mainpage from "./pages/Mainpage.js";
+import MainPage from "./pages/MainPage.js";
 import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage.js";
 import SettingPage from "./pages/SettingPage";
