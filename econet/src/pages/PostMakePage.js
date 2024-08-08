003d6eaf35be72273c4d4c9aa080ec160002e4b3
05ovo2e <jiwonpark2229@gmail.com>
2024-08-08 23:00:54 +0900

    Conflict Postmake

--- econet/src/pages/PostMakePage.js
+++ econet/src/pages/PostMakePage.js
@@ -147,11 +147,7 @@ const SubmitButton = styled.button`
 function PostMakePage() {
   // const isLoggedIn = useRecoilValue(isLoggedInState); // Recoil 상태 읽기
 
-<<<<<<< HEAD
   const navigate = useNavigate();
-=======
-const navigate = useNavigate();
->>>>>>> main
 
   const [clubInfo, setClubInfo] = useState({
     name: "",
