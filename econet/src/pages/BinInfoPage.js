240f9a48d8806f48964a5f7b84ea16eb2a8f0a6d
박지원 <jiwonpark2229@gmail.com>
2024-08-13 22:03:13 +0900

    Update BinInfoPage.js

--- econet/src/pages/BinInfoPage.js
+++ econet/src/pages/BinInfoPage.js
@@ -60,7 +60,7 @@ const LocationInfo = styled.div`
 `;
 
 function BinInfoPage() {
-  const baseUrl = "https://api.econet-us.site/";
+  const baseUrl = "http://13.124.235.155:8000/";
 
   const [binData, setBinData] = useState(null);
   const [loading, setLoading] = useState(true);
